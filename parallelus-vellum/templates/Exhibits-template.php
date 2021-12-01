e2bea478b6a57d7cc7a97a23b659c7e92c14159a
v3ceban <v3ceban@gmail.com>
2021-12-01 14:18:48 -0800

    2

--- parallelus-vellum/templates/Exhibits-template.php
+++ parallelus-vellum/templates/Exhibits-template.php
@@ -5,30 +5,55 @@
  */
 ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-            <?php /* Featured image displays here (full, large, medium_large, medium, or thumbnail) */ ?>
-            <?php the_post_thumbnail('medium') ?>
-            <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-            <p><?php the_time('F jS, Y') ?>
-                <?php /* by <?php the_author() ?> */ ?>
-            </p>
-            <div class="entry">
-                <?php the_content('Read More...'); ?>
-            </div>
-            <p class="postmetadata">
-                <?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?> <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-            </p>
-        </div><?php /* end post class div */ ?>
-    <?php endwhile; ?>
-    <?php /* Navigation (shows if 10+ posts by default) */ ?>
-    <div class="navigation">
-        <div class="alignleft"><?php next_posts_link('&laquo; Older Posts') ?></div>
-        <div class="alignright"><?php previous_posts_link('Newer Posts &raquo;') ?></div>
-    </div>
-<?php else : ?>
-    <h2>Not Found</h2>
-    <p>Sorry, but you are looking for something that isn't here.</p>
-    <?php get_search_form(); ?>
-<?php endif;
-wp_reset_query(); ?>
+<?php global $custom_query, $column_left, $column_right, $headerClass;
+/**
+ * 
+ * The default template for displaying content. Used for post formats: standard and image, Views: blog, index, archive, search and single posts.
+ *
+ */
+
+// Title (for everything except image left layout)
+if (!$column_right && (!get_post_format() || get_post_format() == 'image')) {
+    theme_post_title();
+}
+
+// Image size
+$shortcode = (isset($custom_query->query)) ? $custom_query->query : false;
+$size  = get_post_image_size('post-thumbnail', $shortcode);
+$media = false;
+if (is_single() && get_options_data('blog-options', 'single-post-image', 'false') !== 'true') {
+    $media =  false;
+} else {
+    if (is_array($size)) {
+        $thumb = get_post_thumbnail_id($post->ID);
+        $crop = ($size[0] == 0 || $size[1] == 0) ? false : true;
+
+        if (function_exists('vt_resize') && $thumb) {
+            $image = vt_resize($thumb, '', $size[0], $size[1], $crop);
+            $media = '<img src="' . $image['url'] . '" width="' . $image['width'] . '" height="' . $image['height'] . '">';
+        }
+    } else {
+        $media = get_the_post_thumbnail($post->ID, $size);
+    }
+}
+
+// Content
+if ($column_right) :
+    // If using columns and there is a header
+    if ($headerClass) { ?>
+        <div class="span<?php echo  $column_right ?>">
+        <?php
+    }
+
+    // Post title
+    theme_post_title();
+endif;
+
+// Post Content
+theme_post_content();
+
+
+if ($column_right && $headerClass) : ?>
+        </div><!-- .span# -->
+    <?php
+endif; ?>
