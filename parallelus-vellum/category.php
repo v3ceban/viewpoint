23dc868ac0214c301ac644d4d0210f43408f13b9
v3ceban <v3ceban@gmail.com>
2021-12-01 15:19:20 -0800

    styling

--- parallelus-vellum/category.php
+++ parallelus-vellum/category.php
@@ -13,25 +13,27 @@ get_header(); ?>
 		</header><!-- .archive-header -->
 
 		<?php /* Main Content loop that gets content for the page */ ?>
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-					<?php /* Featured image displays here (full, large, medium_large, medium, or thumbnail) */ ?>
-					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-					<?php the_post_thumbnail('medium') ?>
-				</div><?php /* end post class div */ ?>
-			<?php endwhile; ?>
-			<?php /* Navigation (shows if 10+ posts by default) */ ?>
-			<div class="navigation">
-				<div class="alignleft"><?php next_posts_link('&laquo; Older Posts') ?></div>
-				<div class="alignright"><?php previous_posts_link('Newer Posts &raquo;') ?></div>
-			</div>
-		<?php else : ?>
-			<h2>Not Found</h2>
-			<p>Sorry, but you are looking for something that isn't here.</p>
-			<?php get_search_form(); ?>
-		<?php endif;
-		wp_reset_query(); ?>
-		<?php /* The loop ends here */ ?>
+		<section class="cat-container">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+						<?php /* Featured image displays here (full, large, medium_large, medium, or thumbnail) */ ?>
+						<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						<?php the_post_thumbnail('medium') ?>
+					</div><?php /* end post class div */ ?>
+				<?php endwhile; ?>
+				<?php /* Navigation (shows if 10+ posts by default) */ ?>
+				<div class="navigation">
+					<div class="alignleft"><?php next_posts_link('&laquo; Older Posts') ?></div>
+					<div class="alignright"><?php previous_posts_link('Newer Posts &raquo;') ?></div>
+				</div>
+			<?php else : ?>
+				<h2>Not Found</h2>
+				<p>Sorry, but you are looking for something that isn't here.</p>
+				<?php get_search_form(); ?>
+			<?php endif;
+			wp_reset_query(); ?>
+			<?php /* The loop ends here */ ?>
+		</section>
 	</div><!-- #content -->
 </section><!-- #primary -->
 
