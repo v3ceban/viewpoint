263da96491fef3625dd7d79e6e7dd3637acb5a4f
v3ceban <v3ceban@gmail.com>
2021-12-01 14:23:10 -0800

    exhibits template

--- parallelus-vellum/templates/Exhibits-template.php
+++ parallelus-vellum/templates/Exhibits-template.php
@@ -1,74 +1,45 @@
-<?php
-
+<?php global $custom_query, $column_left, $column_right;
 /**
  * Template Name: Exhibits Template
  */
-?>
 
-<?php global $custom_query, $wp_query, $headerClass, $column_left, $paged;
+$fromShortcode = ($custom_query) ? true : false;
 
-// Check for a custom query, typically sent by a shortcode
-$the_query = (!$custom_query) ? $wp_query : $custom_query;
+// no columns inside <article> for this layout
+$column_left  = false;
+$column_right = false;
 
-if ($the_query->have_posts()) :
+if ($fromShortcode) {
 
-    // Show Blog Posts
-    // ------------------------------------------------------------------
+    // For shortcode just include blog posts loop
+    get_template_part('templates/blog');
+} else {
 
-    // Loop through the results and print each. 
-    while ($the_query->have_posts()) : $the_query->the_post();
-        $headerClass = false; // clean start for each loop 
-        $post_class = ($column_left) ? 'post-image-left' : '';
-?>
+    // Paging doesn't work for page templates set as home page. This is a workaround.
+    $paged = ($paged) ? $paged : get_query_var('page');
+    if ($paged < 1) {
+        $paged = 1;
+    }
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class($post_class); ?>>
-            <div class="post-row">
-                <div class="row-fluid">
-                    <?php
-                    // Use specific post-format template: aside, image, gallery, etc...
-                    get_template_part('templates/post', get_post_format());
-                    ?>
-                </div>
-            </div><!-- .row-fluid -->
-        </article><!-- #post -->
+    // Setup the query 
+    $params = array(
+        'post_type' => 'post',
+        'paged' => $paged,
+        'post_excerpts' => get_options_data('blog-options', 'post-excerpts', 'false')
+    );
+    $custom_query = new WP_Query($params);
 
-    <?php endwhile;
+    // Include full content structure
+    get_header(); ?>
 
-    // Pagination
-    $paging = (isset($custom_query->query['paging']) && $custom_query->query['paging'] == 'false') ? false : true;
-    if ($paging) : get_pagination($the_query);
-    endif;
+    <div id="primary" class="site-content">
+        <div id="content" role="main">
+            <?php get_template_part('templates/blog'); ?>
+        </div><!-- #content -->
+    </div><!-- #primary -->
 
-    // clean up
-    unset($the_query);
-    if (isset($custom_query)) : unset($custom_query);
-    endif;
+<?php
 
-else :
-
-    // No Posts Found
-    // ------------------------------------------------------------------ 
-
-    ?>
-    <article id="post-0" class="post no-results not-found">
-        <header class="entry-header">
-            <h1 class="entry-title"><?php _e('Sorry, no blog posts were found.', 'framework'); ?></h1>
-        </header>
-        <?php if (current_user_can('edit_posts')) :
-            // No posts message for users with permissions 
-        ?>
-            <div class="entry-content">
-                <p><?php printf(__('If you have not already published some blog posts, you can get started now. &nbsp; <a href="%s"><i class="fa fa-pencil-square"></i> Create a blog post</a>.', 'framework'), admin_url('post-new.php')); ?></p>
-            </div><!-- .entry-content -->
-        <?php else :
-            // No posts message to public. 
-        ?>
-            <div class="entry-content">
-                <p><?php _e('We did not find any posts. Try searching for the content or check in another area of the site. Thank you.', 'framework'); ?></p>
-                <?php get_search_form(); ?>
-            </div><!-- .entry-content -->
-        <?php endif; ?>
-    </article><!-- #post-0 -->
-
-<?php endif; // end have_posts() check 
-?>
+    get_sidebar();
+    get_footer();
+} ?>
