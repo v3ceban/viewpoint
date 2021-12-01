0614bb954ac3e9d8811241f141cdb31e3cef3701
v3ceban <v3ceban@gmail.com>
2021-12-01 12:37:27 -0800

    categories

--- parallelus-vellum/templates/post-link.php
+++ parallelus-vellum/templates/post-link.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying posts in the Link post format
  */
@@ -6,16 +7,16 @@
 
 <a href="<?php echo esc_url(get_post_meta($post->ID, 'postformat_link_url', true)); ?>" title="<?php echo esc_attr(get_the_title()); ?>" target="_blank">
 	<header class="post-header">
-		<div class="post-symbol"><i class="fa fa-link"></i></div><span class="post-format-label"><?php _e('Link', 'framework') ?></span>
+		<div class="post-symbol"><i class="fa fa-link"></i></div><span class="post-format-label"><?php the_category(', ') ?></span>
 		<h1 class="entry-title">
 			<?php echo get_the_title(); ?>
 		</h1>
 		<span class="sub-title"><?php echo get_post_meta($post->ID, 'postformat_link_url', true) ?> <i class="fa fa-external-link-square"></i></span>
 	</header>
 </a>
-<?php 
+<?php
 
-	// Post Content
-	theme_post_content();
+// Post Content
+theme_post_content();
 
 ?>
