aca9876df1adc85d2fda080692cadb08635f3a04
v3ceban <v3ceban@gmail.com>
2021-12-03 14:25:34 -0800

    cat-styles

--- parallelus-vellum/archive.php
+++ parallelus-vellum/archive.php
@@ -5,39 +5,46 @@
 
 get_header(); ?>
 
-	<section id="primary" class="site-content">
-		<div id="content" role="main">
-			<header class="archive-header">
-				<h1 class="archive-title"><?php
-					if ( is_day() ) :
-						printf( __( 'Archives: %s', 'framework' ), get_the_date() );
-					elseif ( is_month() ) :
-						printf( __( 'Archives: %s', 'framework' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'framework' ) ) );
-					elseif ( is_year() ) :
-						printf( __( 'Archives: %s', 'framework' ), get_the_date( _x( 'Y', 'yearly archives date format', 'framework' ) ) );
-					else :
-						_e( 'Archives', 'framework' );
-					endif;
-				?></h1>
-			</header><!-- .archive-header -->
+<section id="primary" class="site-content">
+	<div id="content" role="main">
+		<header class="archive-header">
+			<h1 class="archive-title"><?php
+										if (is_day()) :
+											printf(__('Archives: %s', 'framework'), get_the_date());
+										elseif (is_month()) :
+											printf(__('Archives: %s', 'framework'), get_the_date(_x('F Y', 'monthly archives date format', 'framework')));
+										elseif (is_year()) :
+											printf(__('Archives: %s', 'framework'), get_the_date(_x('Y', 'yearly archives date format', 'framework')));
+										else :
+											_e('Archives', 'framework');
+										endif;
+										?></h1>
+		</header><!-- .archive-header -->
 
-			<?php 
-			
-			if ( have_posts() ) :
-
-				// The default query for this page
-				$args = $wp_query->query;
-
-				// Get the blog template
-				get_theme_blog_template( $args );
-
-			else : 
-				get_template_part( 'content', 'none' ); 
-			endif; 
-
-			?>
-		</div><!-- #content -->
-	</section><!-- #primary -->
+		<?php /* Main Content loop that gets content for the page */ ?>
+		<main class="main-container">
+			<section class="cat-container">
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+							<?php /* Featured image displays here (full, large, medium_large, medium, or thumbnail) */ ?>
+							<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail('medium_large') ?></a>
+							<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+							<p><?php the_excerpt(); ?></p>
+							<div class="cat-footer">
+								<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">Read More...</a>
+							</div>
+						</div><?php /* end post class div */ ?>
+					<?php endwhile; ?>
+				<?php else : ?>
+					<section id="not-found">
+						<h2>Not Found</h2>
+						<p>Sorry, but you are looking for something that isn't here.</p>
+						<?php get_search_form(); ?>
+					</section>
+				<?php endif;
+				wp_reset_query(); ?>
+	</div><!-- #content -->
+</section><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
