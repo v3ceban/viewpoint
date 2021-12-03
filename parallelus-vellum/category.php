fdd415f7db713a6f4f87703fc761f484f2c131c2
v3ceban <v3ceban@gmail.com>
2021-12-03 13:14:47 -0800

    cat-aside

--- parallelus-vellum/category.php
+++ parallelus-vellum/category.php
@@ -18,7 +18,7 @@ get_header(); ?>
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 							<?php /* Featured image displays here (full, large, medium_large, medium, or thumbnail) */ ?>
-							<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail('medium') ?></a>
+							<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail('medium_large') ?></a>
 							<div class="cat-text-content">
 								<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 								<p><?php the_excerpt(); ?></p>
