386d67870c38466ca76446bcaa0aa7b7ab6f8f09
v3ceban <v3ceban@gmail.com>
2021-12-03 14:19:12 -0800

    cat-styles

--- parallelus-vellum/functions.php
+++ parallelus-vellum/functions.php
@@ -297,10 +297,10 @@ function basic_widget_init()
 
 add_action('widgets_init', 'basic_widget_init');
 
-// Filter except length to 50 words.
+// Filter except length to 40 words.
 // tn custom excerpt length
 function tn_custom_excerpt_length($length)
 {
-	return 50;
+	return 40;
 }
 add_filter('excerpt_length', 'tn_custom_excerpt_length', 999);
