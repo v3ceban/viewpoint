3278d5b4e3c6a9a0fa46ea92fe62aa8b5a910380
v3ceban <v3ceban@gmail.com>
2021-12-03 19:27:43 -0800

    cats

--- parallelus-vellum/archive.php
+++ parallelus-vellum/archive.php
@@ -31,7 +31,8 @@ get_header(); ?>
 							<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 							<p><?php the_excerpt(); ?></p>
 							<div class="cat-footer">
-								<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">Read More...</a>
+								<?php the_category(' ') ?>
+								<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">Read More...</a>
 							</div>
 						</div><?php /* end post class div */ ?>
 					<?php endwhile; ?>
