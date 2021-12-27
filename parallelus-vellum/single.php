0a1458ae4036f26991e3e739fb0fa16828b5ded6
v3ceban <v3ceban@gmail.com>
2021-12-27 11:39:21 -0800

    custom fields

--- parallelus-vellum/single.php
+++ parallelus-vellum/single.php
@@ -66,8 +66,10 @@ get_header(); ?>
 							<p>Artist Reception: <?php the_field('artist_reception_date'); ?>
 								<?php if (get_field('artist_reception_time_from')) : echo (', ');
 									the_field('artist_reception_time_from');
-								endif; ?> to <?php if (get_field('artist_reception_time_to')) : the_field('artist_reception_time_to');
-												endif; ?></p>
+								endif; ?>
+								<?php if (get_field('artist_reception_time_to')) : echo ('to ');
+									the_field('artist_reception_time_to');
+								endif; ?></p>
 						<?php endif; ?>
 					</aside>
 				</main>
