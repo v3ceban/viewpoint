65896f2f96da9249a8f85af06020a41ead418cd6
v3ceban <v3ceban@gmail.com>
2021-12-27 10:43:45 -0800

    custom fields

--- parallelus-vellum/single.php
+++ parallelus-vellum/single.php
@@ -1,48 +1,62 @@
 <?php
+
 /**
  * The Template for displaying an individual post.
  */
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
+<div id="primary" class="site-content">
+	<div id="content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php 
-					global $has_vc_posts_list, $post_id_has_vc_posts_list;
-					$post_id_has_vc_posts_list = get_the_ID();
-					$has_vc_posts_list = has_shortcode(get_the_content(), 'blog');
+		<?php while (have_posts()) : the_post(); ?>
+			<?php
+			global $has_vc_posts_list, $post_id_has_vc_posts_list;
+			$post_id_has_vc_posts_list = get_the_ID();
+			$has_vc_posts_list = has_shortcode(get_the_content(), 'blog');
+			?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php
+				// Post Navigation (next/previous links)
+				//................................................................
+				$showPostNav = (get_options_data('blog-options', 'show-post-navigation') == 'true') ? true : false;
+				if (isset($showPostNav) && $showPostNav == 'true') {
+					// Show navigation
+					next_and_previous_post_navigation();
+				}
 				?>
+				<div class="row-fluid theMainPostContent">
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php
-						// Post Navigation (next/previous links)
-						//................................................................
-						$showPostNav = (get_options_data('blog-options', 'show-post-navigation') == 'true') ? true : false;
-						if (isset($showPostNav) && $showPostNav == 'true') {
-							// Show navigation
-							next_and_previous_post_navigation();
-						}
-					?>
-					<div class="row-fluid">
+					<?php get_template_part('templates/post', get_post_format()); ?>
 
-						<?php get_template_part( 'templates/post', get_post_format() ); ?>
+				</div><!-- .row-fluid -->
 
-					</div><!-- .row-fluid -->
+				<aside>
+					<?php if (get_field('artists_name')) : ?>
+						<h2><?php the_field('artists_name'); ?></h2>
+					<?php endif; ?>
+					<?php if (get_field('artists_bio')) : ?>
+						<p><?php the_field('artists_bio'); ?></p>
+					<?php endif; ?>
+					<?php if (get_field('artists_photo')) : ?>
+						<?php the_field('artists_photo'); ?>
+					<?php endif; ?>
+				</aside>
 
-					<?php 
-					
-					// Show comments
-					//................................................................
-					comments_template( '', true ); ?>
-				
-				</article><!-- #post -->
+				<?php
 
-			<?php endwhile; // end of the loop. ?>
+				// Show comments
+				//................................................................
+				comments_template('', true); ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+			</article><!-- #post -->
+
+		<?php endwhile; // end of the loop. 
+		?>
+
+	</div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
