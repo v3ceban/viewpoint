5a8c5276a783557dfd6bc1a0ebdb0f4220a602cc
v3ceban <v3ceban@gmail.com>
2022-01-18 15:30:22 -0800

    stuff

--- parallelus-vellum/template-exhibits.php
+++ parallelus-vellum/template-exhibits.php
@@ -35,9 +35,9 @@ get_header(); ?>
                                 <p class="dates">Date: <?php the_field('dates_from'); ?></p>
                             <?php endif; ?>
                             <?php if (get_field('2nd_reception_date')) : ?>
-                                <p class="dates">2nd Satruday Opening: <?php the_field('2nd_reception_date'); ?><?php if (get_field('2nd_reception_time_from')) : echo (', ');
-                                                                                                                    the_field('2nd_reception_time_from');
-                                                                                                                endif; ?>
+                                <p class="dates">2nd Saturday Reception: <?php the_field('2nd_reception_date'); ?><?php if (get_field('2nd_reception_time_from')) : echo (', ');
+                                                                                                                        the_field('2nd_reception_time_from');
+                                                                                                                    endif; ?>
                                     <?php if (get_field('2nd_reception_time_to') and get_field('2nd_reception_time_from')) : echo ('to ');
                                         the_field('2nd_reception_time_to');
                                     endif; ?></p>
