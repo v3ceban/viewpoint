4f06e43392c360f392b2d7216505d6024bc59271
v3ceban <v3ceban@gmail.com>
2022-01-17 20:46:48 -0800

    templates

--- parallelus-vellum/exhibits-template.php
+++ parallelus-vellum/exhibits-template.php
@@ -17,7 +17,7 @@ get_header(); ?>
         <?php /* if intended to use on a static page set to 'page' instead. */ ?>
         <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
         <?php /* Set cat= to the category number to filter posts by category, set posts_per_page=-1 to display all or x to display x, set paged to fix pagination*/ ?>
-        <?php query_posts('cat=33`&posts_per_page=6&paged=' . $paged); ?>
+        <?php query_posts('cat=33&posts_per_page=6&paged=' . $paged); ?>
 
         <?php /* Main Content loop that gets content for the page */ ?>
         <main class="main-container">
