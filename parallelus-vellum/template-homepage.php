ed4cde9d38ccfadde51ac9f93029bf2ae0b11789
v3ceban <v3ceban@gmail.com>
2022-01-19 11:51:58 -0800

    cal

--- parallelus-vellum/template-homepage.php
+++ parallelus-vellum/template-homepage.php
@@ -13,20 +13,20 @@ get_header(); ?>
         <?php /* Set cat= to the category number to filter posts by category, set posts_per_page=-1 to display all or x to display x*/ ?>
         <?php query_posts('cat=28,-33&posts_per_page=2'); ?>
 
-        <?php /* Loop for posts */ ?>
-        <h2>CURRENT EXHIBITS</h2>
-        <main class="main-container">
+        <main class="home-main-container">
             <section class="home-page-content">
-                <?php while (have_posts()) : the_post(); // loop for page content 
-                ?>
+                <?php /* Loop for page content */ ?>
+                <?php while (have_posts()) : the_post(); ?>
 
                     <?php get_template_part('templates/page'); ?>
                     <?php comments_template('', true); ?>
 
-                <?php endwhile; // end of the loop. 
-                ?>
+                <?php endwhile; ?>
+                <?php /* End of the page content loop */ ?>
             </section>
-            <section class="cat-container">
+            <h2>CURRENT EXHIBITS</h2>
+            <?php /* Loop for posts */ ?>
+            <section class="home-page-posts">
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                         <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
                             <?php /* Featured image displays here (full, large, medium_large, medium, or thumbnail) */ ?>
@@ -64,10 +64,10 @@ get_header(); ?>
                 <?php endif;
                 wp_reset_query(); ?>
             </section>
-            <?php /* The loop ends here */ ?>
-            <aside class="cat-widget-area">
+            <?php /* End of the posts loop */ ?>
+            <aside class="home-widget-area">
                 <?php if (!dynamic_sidebar('home-aside-area')) : ?>
-                    <?php /* Footer content if widgets are not being used */ ?>
+                    <?php /* Widget content if widgets are not being used */ ?>
                     <h3>Add an empty widget in this widget area to hide this</h3>
                 <?php endif; ?>
             </aside>
