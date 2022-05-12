1ff2356b0e3b3c21d13fe4a0ddd80ddf8c5cca78
v3ceban <v3ceban@gmail.com>
2022-05-12 13:44:04 -0700

    cat-check

--- parallelus-vellum/single.php
+++ parallelus-vellum/single.php
@@ -31,95 +31,94 @@ get_header(); ?>
 						<?php get_template_part('templates/post', get_post_format()); ?>
 					</div>
 					<!--Post Content-->
-					<aside id="singleAside">
-						<?php if (get_field('name')) : ?>
-							<h3><?php the_field('name'); ?></h3>
-							<hr>
-						<?php endif; ?>
-						<div class="biography">
-							<h3>Bio</h3>
-							<p><?php $categories = get_the_category();
-								if (($categories[0]->name) !== "Call for Entries") {
-									echo esc_html($categories[0]->name);
-								} ?></p>
-							<?php if (get_field('photo')) : ?>
-								<img src="<?php the_field('photo'); ?>" alt="<?php the_field('name'); ?> bio" loading="lazy" class="alignleft bioImage">
-							<?php endif; ?>
-							<?php if (get_field('bio')) : ?>
-								<p><?php the_field('bio'); ?></p>
+					<?php $categories = get_the_category();
+					if (($categories[0]->name) !== "Call for Entries") : ?>
+						<aside id="singleAside">
+							<?php if (get_field('name')) : ?>
+								<h3><?php the_field('name'); ?></h3>
 								<hr>
 							<?php endif; ?>
-						</div>
-						<h3>Details</h3>
-						<ul>
-							<?php if (get_field('dates_from')) : ?>
-								<li>Date: <?php the_field('dates_from'); ?>
-									<?php if (get_field('dates_to')) : echo ('to ');
-										the_field('dates_to');
-									endif; ?></li>
+							<div class="biography">
+								<h3>Bio</h3>
+								<?php if (get_field('photo')) : ?>
+									<img src="<?php the_field('photo'); ?>" alt="<?php the_field('name'); ?> bio" loading="lazy" class="alignleft bioImage">
+								<?php endif; ?>
+								<?php if (get_field('bio')) : ?>
+									<p><?php the_field('bio'); ?></p>
+									<hr>
+								<?php endif; ?>
+							</div>
+							<h3>Details</h3>
+							<ul>
+								<?php if (get_field('dates_from')) : ?>
+									<li>Date: <?php the_field('dates_from'); ?>
+										<?php if (get_field('dates_to')) : echo ('to ');
+											the_field('dates_to');
+										endif; ?></li>
+								<?php endif; ?>
+
+								<?php if (get_field('gallery_space')) : ?>
+									<li>Gallery Space: <?php the_field('gallery_space'); ?></li>
+								<?php endif; ?>
+
+								<?php if (get_field('2nd_reception_date')) : ?>
+									<li>2nd Satruday Opening: <?php the_field('2nd_reception_date'); ?><?php if (get_field('2nd_reception_time_from')) : echo (', ');
+																											the_field('2nd_reception_time_from');
+																										endif; ?>
+										<?php if (get_field('2nd_reception_time_to') and get_field('2nd_reception_time_from')) : echo ('to ');
+											the_field('2nd_reception_time_to');
+										endif; ?></li>
+								<?php endif; ?>
+
+								<?php if (get_field('artist_reception_date')) : ?>
+									<li>Artist Reception: <?php the_field('artist_reception_date'); ?><?php if (get_field('artist_reception_time_from')) : echo (', ');
+																											the_field('artist_reception_time_from');
+																										endif; ?>
+										<?php if (get_field('artist_reception_time_to') and get_field('artist_reception_time_from')) : echo ('to ');
+											the_field('artist_reception_time_to');
+										endif; ?></li>
+								<?php endif; ?>
+
+								<?php if (get_field('workshop_time_from')) : ?>
+									<li>Time: <?php the_field('workshop_time_from'); ?><?php if (get_field('workshop_time_to') and get_field('workshop_time_to')) : echo (' to ');
+																							the_field('workshop_time_to');
+																						endif; ?></li>
+								<?php endif; ?>
+
+								<?php if (get_field('location')) : ?>
+									<li>Location: <?php the_field('location'); ?></li>
+								<?php endif; ?>
+
+								<?php if (get_field('cost') && get_field('member_cost') == null) : ?>
+									<li>Cost: $<?php the_field('cost'); ?></li>
+								<?php elseif (get_field('cost') === '0') : ?>
+									<li>Free for everyone!</li>
+								<?php endif; ?>
+
+								<?php if (get_field('cost') && get_field('member_cost') === '0') : ?>
+									<li>Non-Member Cost: $<?php the_field('cost'); ?></li>
+									<li>Member Cost: Free!</li>
+								<?php endif; ?>
+
+								<?php if (get_field('cost') && get_field('member_cost')) : ?>
+									<li>Non-Member Cost: $<?php the_field('cost'); ?></li>
+									<li>Member Cost: $<?php the_field('member_cost'); ?></li>
+								<?php endif; ?>
+							</ul>
+							<hr>
+							<?php if (get_field('registration_link')) : ?>
+								<a href="<?php the_field('registration_link'); ?>">
+									<h3 class="regButton">Register Here</h3>
+								</a>
 							<?php endif; ?>
 
-							<?php if (get_field('gallery_space')) : ?>
-								<li>Gallery Space: <?php the_field('gallery_space'); ?></li>
+							<?php if (get_field('sales_link')) : ?>
+								<a href="<?php the_field('sales_link'); ?>">
+									<h3 class="regButton">Online Gallery and Sales</h3>
+								</a>
 							<?php endif; ?>
-
-							<?php if (get_field('2nd_reception_date')) : ?>
-								<li>2nd Satruday Opening: <?php the_field('2nd_reception_date'); ?><?php if (get_field('2nd_reception_time_from')) : echo (', ');
-																										the_field('2nd_reception_time_from');
-																									endif; ?>
-									<?php if (get_field('2nd_reception_time_to') and get_field('2nd_reception_time_from')) : echo ('to ');
-										the_field('2nd_reception_time_to');
-									endif; ?></li>
-							<?php endif; ?>
-
-							<?php if (get_field('artist_reception_date')) : ?>
-								<li>Artist Reception: <?php the_field('artist_reception_date'); ?><?php if (get_field('artist_reception_time_from')) : echo (', ');
-																										the_field('artist_reception_time_from');
-																									endif; ?>
-									<?php if (get_field('artist_reception_time_to') and get_field('artist_reception_time_from')) : echo ('to ');
-										the_field('artist_reception_time_to');
-									endif; ?></li>
-							<?php endif; ?>
-
-							<?php if (get_field('workshop_time_from')) : ?>
-								<li>Time: <?php the_field('workshop_time_from'); ?><?php if (get_field('workshop_time_to') and get_field('workshop_time_to')) : echo (' to ');
-																						the_field('workshop_time_to');
-																					endif; ?></li>
-							<?php endif; ?>
-
-							<?php if (get_field('location')) : ?>
-								<li>Location: <?php the_field('location'); ?></li>
-							<?php endif; ?>
-
-							<?php if (get_field('cost') && get_field('member_cost') == null) : ?>
-								<li>Cost: $<?php the_field('cost'); ?></li>
-							<?php elseif (get_field('cost') === '0') : ?>
-								<li>Free for everyone!</li>
-							<?php endif; ?>
-
-							<?php if (get_field('cost') && get_field('member_cost') === '0') : ?>
-								<li>Non-Member Cost: $<?php the_field('cost'); ?></li>
-								<li>Member Cost: Free!</li>
-							<?php endif; ?>
-
-							<?php if (get_field('cost') && get_field('member_cost')) : ?>
-								<li>Non-Member Cost: $<?php the_field('cost'); ?></li>
-								<li>Member Cost: $<?php the_field('member_cost'); ?></li>
-							<?php endif; ?>
-						</ul>
-						<hr>
-						<?php if (get_field('registration_link')) : ?>
-							<a href="<?php the_field('registration_link'); ?>">
-								<h3 class="regButton">Register Here</h3>
-							</a>
-						<?php endif; ?>
-
-						<?php if (get_field('sales_link')) : ?>
-							<a href="<?php the_field('sales_link'); ?>">
-								<h3 class="regButton">Online Gallery and Sales</h3>
-							</a>
-						<?php endif; ?>
-					</aside>
+						</aside>
+					<?php endif; ?>
 				</main>
 				<?php
 
