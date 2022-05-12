46dc85b1dbe55c3b8154c31da47484b5af0a6467
v3ceban <v3ceban@gmail.com>
2022-05-12 13:37:41 -0700

    cat-check

--- parallelus-vellum/single.php
+++ parallelus-vellum/single.php
@@ -39,7 +39,7 @@ get_header(); ?>
 						<div class="biography">
 							<h3>Bio</h3>
 							<p><?php $categories = get_the_category();
-								if (($categories) == "Call for Entries") {
+								if (($categories) = "Call for Entries") {
 									echo '<p>this one</p>';
 								} ?></p>
 							<?php if (get_field('photo')) : ?>
